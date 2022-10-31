0bc6b840eafeb7e03623707b8dff46684c7e10f0
mrmerritt <emerritt@almasd.net>
2022-10-31 16:36:36 -0500

    added HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+
+
+#This is a comment in python
+
+'This is a string that acts as a comment'
+
+'''
+   This is a docstring in python. 
+   More on this later. But it can be used 
+   as a multi-line comment
+'''
+
+#Your first program
+print("Hello World")
